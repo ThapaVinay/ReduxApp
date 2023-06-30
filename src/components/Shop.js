76a94dd9694c22ecfc71ae44120c7d6dbd87e3f9
ThapaVinay <thechampthapa@gmail.com>
2023-06-30 10:36:31 +0530

    initial commit basic react app setup

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h3>Buy WHEY protien for 60rs</h3>
+            <button className="btn btn-primary mx-2"> - </button>
+            Add this item to Cart
+            <button className="btn btn-primary mx-2"> + </button>
+        </div>
+    )
+}
+
+export default Shop
